2671ae1f6c04ce9f7acd24a0316e6d8022883c72
Harshalgbj <125290125+harshalgangurde44@users.noreply.github.com>
2024-03-27 16:07:38 +0530

    add

--- day8/quiz/masai-cp-problems-2980-1959-XLaAiW-82bcc1b31b899069ce72c72f4e7f646317c5badf/src/Components/AddQuiz.tsx
+++ day8/quiz/masai-cp-problems-2980-1959-XLaAiW-82bcc1b31b899069ce72c72f4e7f646317c5badf/src/Components/AddQuiz.tsx
@@ -31,44 +31,49 @@ export const AddQuiz: React.FC = () => {
         className="problem-statement"
         value={problem}
         onChange={(e) => setProblem(e.target.value)}
+        placeholder="question"
       />
       <input
         className="option-a"
         value={optionA}
         onChange={(e) => setOptionA(e.target.value)}
+        placeholder="option-a"
       />
       <input
         className="option-b"
         value={optionB}
         onChange={(e) => setOptionB(e.target.value)}
+        placeholder="option-b"
       />
       <input
         className="option-c"
         value={optionC}
         onChange={(e) => setOptionC(e.target.value)}
+        placeholder="option-c"
       />
       <input
         className="option-d"
         value={optionD}
         onChange={(e) => setOptionD(e.target.value)}
+        placeholder="option-c"
       />
       <select
         className="correct-option"
         value={correctOption}
         onChange={(e) => setCorrectOption(e.target.value)}
       >
-        <input
-          className="weightage"
-          type="number"
-          value={weightage}
-          onChange={(e) => setWeightage(Number(e.target.value))}
-        />
         <option value="">Select Correct Option</option>
         <option value="a">A</option>
         <option value="b">B</option>
         <option value="c">C</option>
         <option value="d">D</option>
       </select>
+      <input
+        className="weightage"
+        type="number"
+        value={weightage}
+        onChange={(e) => setWeightage(Number(e.target.value))}
+      />
       <button className="submit-form" type="submit">
         Submit
       </button>
